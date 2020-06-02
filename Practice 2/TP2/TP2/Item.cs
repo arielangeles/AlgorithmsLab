81a09909d0a46f58c81f4e2fcb2cd049ad59b8bc
Ariel Angeles <arielangeles123@hotmail.com>
2020-06-02 01:38:07 -0400

    Added Practice 2

---
+++ Practice 2/TP2/TP2/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP2
+{
+    public struct Item
+    {
+        public string Key;
+        public string Value;
+    }
+}
---
+++ Practice 2/TP2/TP2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Find pair such that pairSum = x func */
+            TP2A tp2a = new TP2A();
+            int[] arreglo = { 10, 2, 13, 7, 15 };
+            //int[] arr = { 2 };
+
+            int[] lista = tp2a.FindMinDifference(arreglo);
+
+            if (lista[0] == -1) Console.WriteLine(lista[0]);
+            else Console.WriteLine(string.Join(" - ", lista));
+
+
+            Console.ReadLine();
+
+            TP2B tp2b = new TP2B();
+            Console.WriteLine($"[{string.Join(", ", arreglo)}]");
+            int x = 9;    
+            int[] foundPair = tp2b.FindPair(arreglo, x);
+           
+            if (foundPair[0] == int.MaxValue && foundPair[1] == int.MinValue) Console.WriteLine(-1);
+            else Console.WriteLine($"\nX={x}\n[{string.Join(", ", foundPair)}]");
+
+            Console.ReadLine();
+        }
+    }
+}
