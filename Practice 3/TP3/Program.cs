ead4bf84f6d1ae4c5f94e8f5e2e799001b73c173
Ariel Angeles <arielangeles123@hotmail.com>
2020-06-10 13:43:54 -0400

    Added TP3

---
+++ Practice 3/TP3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TP3B
+            LinkedListNode<int> listNode1 = AddLinkedList();
+            LinkedListNode<int> listNode2 = AddLinkedList();
+
+            Console.WriteLine("LinkedList1:");
+            PrintLinkedList(listNode1);
+
+            Console.WriteLine("\nLinkedList2");
+            PrintLinkedList(listNode2);
+
+            LinkedListNode<int> mergedList = TP3B.MergeLinkedList(listNode1, listNode2);
+
+            Console.WriteLine("\nMergedLinkedList:");
+            PrintLinkedList(mergedList);
+            
+            #endregion
+
+        }
+
+        public static void PrintLinkedList(LinkedListNode<int> ls)
+        {
+            while(ls != null)
+            {
+                Console.Write(ls.Value + " -> ");
+                ls = ls.Next;
+            }            
+        }
+
+        public static LinkedListNode<int> AddLinkedList()
+        {
+            LinkedList<int> ls = new LinkedList<int>();
+            bool finished = false;
+            while (!finished)
+            {
+                Console.WriteLine("Insert the length of the linked list: ");
+                bool isInt = int.TryParse(Console.ReadLine(), out int n);
+
+                if (isInt)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write($"Insert element {i+1}: ");
+                        bool isVal = int.TryParse(Console.ReadLine(), out int val);
+
+                        if (isVal)
+                        {
+                            ls.AddLast(val);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Insert a valid number");
+                            i--;
+                        }
+                    }
+
+                    finished = true;
+                }
+                else
+                {
+                    Console.WriteLine("Insert a valid number");
+                }
+
+                            
+            }
+
+            return ls.First;
+
+        }
+    }
+}
---
+++ Practice 3/TP3/TP3B.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP3
+{
+    public class TP3B
+    {
+
+        public static LinkedListNode<int> MergeLinkedList(LinkedListNode<int> linkedList1, LinkedListNode<int> linkedList2)
+        {
+            LinkedList<int> result = new LinkedList<int>();
+            //LinkedListNode<int> headNode = result.First;
+
+            while (linkedList1 != null && linkedList2 != null)
+            {
+                if (linkedList1.Value < linkedList2.Value)
+                {
+                    result.AddLast(linkedList1.Value);
+                    linkedList1 = linkedList1.Next;
+                }
+                else
+                {
+                    result.AddLast(linkedList2.Value);
+                    linkedList2 = linkedList2.Next;
+                }
+            }
+
+            while (linkedList1 != null)
+            {
+                result.AddLast(linkedList1.Value);
+                linkedList1 = linkedList1.Next;
+            }
+
+            while (linkedList2 != null)
+            {
+                result.AddLast(linkedList2.Value);
+                linkedList2 = linkedList2.Next;
+            }
+
+            return result.First;
+        }
+    }
+}
