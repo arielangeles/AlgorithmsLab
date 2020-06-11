9675f3f295840fa2ebea7783e133557cad0a86e1
Ariel Angeles <arielangeles123@hotmail.com>
2020-06-11 15:40:30 -0400

    Added UnitTestPractice3

---
+++ Practice 3/UnitTestPractice3/UnitTest1.cs
@@ -0,0 +1,123 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP3;
+using System.Collections.Generic;
+using System;
+
+namespace UnitTestPractice3
+{
+    [TestClass]
+    public class TP3BTests
+    {
+        [TestMethod]
+        public void MergeLinkedListTest()
+        {
+            LinkedList<int> ls1 = new LinkedList<int>();
+            LinkedList<int> ls2 = new LinkedList<int>();
+            var random = new Random();
+
+            int length1 = random.Next(10);
+            int length2 = random.Next(10);
+
+            int y = int.MinValue;
+            for (int i = 0; i < length1; i++)
+            {   
+                int val = random.Next(-10, 10);
+                if (val >= y)
+                {
+                    ls1.AddLast(val);
+                    y = val;
+                }
+                else i--;
+            }
+
+            y = int.MinValue;
+            for (int i = 0; i < length2; i++)
+            {
+                int val = random.Next(-10, 10);
+                if (val >= y)
+                {
+                    ls2.AddLast(val);
+                    y = val;
+                }
+                else i--;
+            }
+
+            LinkedListNode<int> list1 = ls1.First;
+            LinkedListNode<int> list2 = ls2.First;
+
+            LinkedListNode<int> mergedList1 = TP3B.MergeLinkedList(list1, list2);
+
+            LinkedListNode<int> mergedList2 = MergeLists(list1, list2);
+
+            Assert.AreEqual(mergedList1, mergedList2, "Ok gracias");
+            
+        }
+
+        public static LinkedListNode<int> MergeLists(LinkedListNode<int> ls1, LinkedListNode<int> ls2)
+        {
+            LinkedList<int> result = new LinkedList<int>();
+
+            int[] arr1 = ConvertFromLinkedListToArray(ls1.List);
+            int[] arr2 = ConvertFromLinkedListToArray(ls2.List);
+
+            int[] resultArr = Merge(arr1, arr2);
+
+            for (int i = 0; i < resultArr.Length; i++)
+            {
+                result.AddLast(resultArr[i]);
+            }
+
+            return result.First;
+        }
+        public static int[] Merge(int[] arr1, int[] arr2)
+        {
+            int totalLength = arr1.Length + arr2.Length;
+            int[] result = new int[totalLength];
+
+
+            int j = 0, i = 0;
+            for (int x = 0; x < totalLength; x++)
+            {
+                if (i < arr1.Length && j < arr2.Length)
+                {
+                    if (arr1[i] < arr2[j])
+                    {
+                        result[x] += arr1[i];
+                        i++;
+                    }
+                    else
+                    {
+                        result[x] += arr2[j];
+                        j++;
+                    }
+
+                }
+                else if (i < arr1.Length)
+                {
+                    result[x] += arr1[i];
+                    i++;
+                }
+                else if (j < arr2.Length)
+                {
+                    result[x] += arr2[j];
+                    j++;
+                }
+            }
+
+            return result;
+        }
+
+        public static int[] ConvertFromLinkedListToArray(LinkedList<int> ls)
+        {
+            int[] arr1 = new int[ls.Count];
+            LinkedListNode<int> lsn = ls.First;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = lsn.Value;
+                lsn = lsn.Next;
+            }
+            return arr1;
+        }
+    }
+
+}
