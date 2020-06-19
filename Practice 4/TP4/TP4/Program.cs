d84f8c53e7eae2302d9bc00a195550d3f8bcf9f8
Ariel <arielangeles123@hotmail.com>
2020-06-18 23:00:43 -0400

    added Practice 4

---
+++ Practice 4/TP4/TP4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace TP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int[] result = countingSort(arr);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        static int[] countingSort(int[] arr)
+        {
+            int[] frequencyArray = new int[100]; // 
+            int[] sorted = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++) frequencyArray[arr[i]]++;
+
+            for (int i = 1; i < frequencyArray.Length; i++) frequencyArray[i] += frequencyArray[i - 1];
+
+            for (int i = arr.Length - 1; i >= 0; i--) 
+            {
+                int c = arr[i];
+                sorted[--frequencyArray[c]] = c;
+            } 
+
+            return sorted;
+
+
+        }
+    }
+}
