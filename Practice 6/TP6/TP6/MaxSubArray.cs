4e3bdfa443250e9cf35d8521e39f3e999cb2d323
Ariel <arielangeles123@hotmail.com>
2020-06-29 18:01:26 -0400

    added TP6

---
+++ Practice 6/TP6/TP6/MaxSubArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP6
+{
+    public class MaxSubArray
+    {
+        public static int[] maxSubarray(int[] arr)
+        {
+            //1
+            int size = arr.Length;
+            int currentMax = 0;
+            int resultMax = int.MinValue;
+
+
+            for (int i = 0; i < size; i++)
+            {
+                currentMax = Math.Max(arr[i], arr[i] + currentMax);
+                if (currentMax > resultMax)
+                {
+                    resultMax = currentMax;
+                }
+            }
+
+            //2
+            Array.Sort(arr);
+            int suma = 0;
+
+            if (arr[size - 1] <= 0)
+                suma = arr[size - 1];
+            else
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    if (arr[x] > 0)
+                        suma += arr[x];
+                }
+            }
+
+            return new int[] { resultMax, suma };
+
+        }
+    }
+}
---
+++ Practice 6/TP6/TP6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> results = new List<int[]>();
+
+            int[] array1 = { -21, -2, -3, -4 };
+            int[] result1 = MaxSubArray.maxSubarray(array1);
+
+            int[] array2 = { 1, 2, 3, 4 };
+            int[] result2 = MaxSubArray.maxSubarray(array2);
+
+            int[] array3 = { 8, -2, 5, -4 };
+            int[] result3 = MaxSubArray.maxSubarray(array3);
+
+            results.Add(result1);
+            results.Add(result2);
+            results.Add(result3);
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item[0]} {item[1]}");
+            }
+        }
+
+       
+    
+    }
+}
